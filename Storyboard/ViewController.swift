21755e0508bdd0c3c4f0f7d1153010837175d12e
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2021-11-20 17:29:43 +0700

    Initial Commit

---
+++ Storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Storyboard
+//
+//  Created by Prana Apsara Wijaya on 20/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
