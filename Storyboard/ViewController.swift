1f5bd7daabe01762015fcbfcefe131dd80fccfb9
Prana Apsara Wijaya <1911510996@student.budiluhur.ac.id>
2021-12-02 07:29:13 +0700

    initial commit

--- Storyboard/ViewController.swift
+++ Storyboard/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var LblCategiry: UILabel!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    @IBOutlet weak var ImgRecommend: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.LblCategiry.layer.masksToBounds = true
+        self.LblCategiry.layer.cornerRadius = 10
     }
 
 
